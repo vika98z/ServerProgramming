e129efcd53c9d5af309f7eb9a1e9bb55ace80900
vika98z <vika98z@mail.ru>
2021-02-27 16:52:44 +0300

    init commit

---
+++ ServerApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpListener httpListener = new HttpListener();
+            httpListener.Prefixes.Add("http://localhost:1234/");
+            httpListener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Ожидание подключений...");
+                HttpListenerContext context = httpListener.GetContext();
+                HttpListenerResponse response = context.Response;
+                HttpListenerRequest request = context.Request;
+
+                string path = request.RawUrl;
+
+                if (path == "/")
+                    path = "/index.html";
+
+                string filename = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\")) + path;
+                filename = filename.Replace("%20", " ");
+
+                if (File.Exists(filename))
+                {
+                    response.StatusCode = 200;
+                    var bytes = File.ReadAllBytes(filename);
+                    response.ContentLength64 = bytes.Length;
+                    response.OutputStream.Write(bytes, 0, bytes.Length);
+                }
+                else
+                    response.StatusCode = 404;
+                response.OutputStream.Close();
+
+                using (StreamWriter writter = File.AppendText("../../../log.log"))
+                {
+                    writter.WriteLine(DateTime.Now + " " + request.RemoteEndPoint.Address + " " + request.HttpMethod + " " + path + " " + response.StatusCode);
+                }
+            }
+        }
+    }
+}
