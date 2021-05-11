ec73e2e04064c7813302323e6a736318446bd301
vika98z <vika98z@mail.ru>
2021-05-11 15:20:43 +0300

    refactoring

--- Lab 5/MVC/Data/DbInitializer.cs
+++ Lab 5/MVC/Data/DbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using MVC.Models;
 
@@ -7,40 +8,42 @@ namespace MVC.Data
   public class DbInitializer
   {
     public static void Initialize(SocialNetworkContext context)
-        {
-            context.Database.EnsureCreated();
+    {
+      context.Database.EnsureCreated();
 
-            // Look for any users.
-            if (context.Users.Any())
-                return;   // DB has been seeded
+      // Look for any users.
+      if (context.Users.Any())
+        return; // DB has been seeded
 
-            var users = new User[]
-            {
-            new User{UserId = 0, UserName= "Carson",FullName="Alexander", Biography = "none"},
-            new User{UserId = 1, UserName= "Meredith",FullName="Alonso", Biography = "none"},
-            new User{UserId = 2, UserName= "Arturo",FullName="Anand", Biography = "none"},
-            new User{UserId = 3, UserName= "Gytis",FullName="Barzdukas", Biography = "none"},
-            new User{UserId = 4, UserName= "Yan",FullName="Li", Biography = "none"},
-            };
-            
-            foreach (User u in users)
-                context.Users.Add(u);
-            
-            context.SaveChanges();
+      var users = new[]
+      {
+        new User {UserId = 0, UserName = "Carson", FullName = "Alexander", Biography = "none"},
+        new User {UserId = 1, UserName = "Meredith", FullName = "Alonso", Biography = "none"},
+        new User {UserId = 2, UserName = "Arturo", FullName = "Anand", Biography = "none"},
+        new User {UserId = 3, UserName = "Gytis", FullName = "Barzdukas", Biography = "none"},
+        new User {UserId = 4, UserName = "Yan", FullName = "Li", Biography = "none"},
+      };
 
-            var posts = new Post[]
-            {
-            new Post{CreatedDate=DateTime.Parse("2005-09-01"),  Text = "some post", User = users[0]},
-            new Post{CreatedDate=DateTime.Parse("2005-09-01"),  Text = "some post", User = users[0]},
-            new Post{CreatedDate=DateTime.Parse("2005-09-01"),  Text = "some post", User = users[3]},
-            new Post{CreatedDate=DateTime.Parse("2005-09-01"),  Text = "some post", User = users[4]},
-            new Post{CreatedDate=DateTime.Parse("2005-09-01"),  Text = "some post", User = users[2]},
-            };
-            
-            foreach (Post p in posts)
-                context.Posts.Add(p);
+      users[0].Followers = new List<User> {users[1], users[2]};
 
-            context.SaveChanges();
-        }
+      foreach (var u in users)
+        context.Users.Add(u);
+
+      context.SaveChanges();
+
+      var posts = new[]
+      {
+        new Post {CreatedDate = DateTime.Parse("2005-09-01"), Text = "some post", User = users[0]},
+        new Post {CreatedDate = DateTime.Parse("2005-09-01"), Text = "some post", User = users[0]},
+        new Post {CreatedDate = DateTime.Parse("2005-09-01"), Text = "some post", User = users[3]},
+        new Post {CreatedDate = DateTime.Parse("2005-09-01"), Text = "some post", User = users[4]},
+        new Post {CreatedDate = DateTime.Parse("2005-09-01"), Text = "some post", User = users[2]},
+      };
+
+      foreach (var p in posts)
+        context.Posts.Add(p);
+
+      context.SaveChanges();
+    }
   }
 }
